e45abca5f7228d5a5ab67da3ab3052e42f68daad
Rutakata <vladpelishenko14@gmail.com>
2023-03-04 19:07:22 +0200

    Auth Warning Popup design update

--- src/components/ClothesDetails/AuthorizationPopup/AuthorizationPopup.jsx
+++ src/components/ClothesDetails/AuthorizationPopup/AuthorizationPopup.jsx
@@ -6,8 +6,14 @@ import style from "./AuthorizationPopup.module.css";
 const AuthorizationPopup = (props) => {
     return <div className={props.isShown ? style.wrapper : style.wrapper_hidden} onClick={props.handlePopup}> 
         <div className={style.container} onClick={(e) => e.stopPropagation()}>
-            <h3>To add product to cart you must have an account and be logged in</h3>
-            <p>You can <Link to='/signin'>log in</Link>|<Link to='/signup'>sign up</Link></p>   
+            <div style={{display: 'flex',justifyContent: 'space-between'}}>
+                <h3 className={style.container__header}>To add product to cart you must have an account and be logged in</h3>
+                <div className={style.container__close} onClick={props.handlePopup}>
+                    <div className={style.container__close_stick}></div>
+                    <div className={style.container__close_stick}></div>
+                </div>
+            </div>
+            <p className={style.container__authLinks}>You can <Link to='/signin'>log in</Link> | <Link to='/signup'>sign up</Link></p>   
         </div>
     </div> 
 }
